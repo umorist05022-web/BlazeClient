b8ca957209e274e709eae690c94340913cbee764
umorist05022-web <umorist05022@gmail.com>
2026-07-19 15:22:11 +0300

    Add client player mixin

---
+++ src/main/java/com/blazeclient/mixin/ClientPlayerMixin.java
@@ -0,0 +1,8 @@
+package com.blazeclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(net.minecraft.client.network.ClientPlayerEntity.class)
+public class ClientPlayerMixin {
+    // Mixin для обработки действий игрока
+}
